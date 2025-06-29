87bcc598f152750d8b10b866e697ff3fc775fd82
melanie <114537207+todayisthorsday@users.noreply.github.com>
2025-06-29 06:16:32 -0230

    Add main CLI menu class with basic structure and entry point

--- src/main/java/com/audiotracker/audiotracker_cli/Main.java
+++ src/main/java/com/audiotracker/audiotracker_cli/Main.java
@@ -2,10 +2,11 @@ package com.audiotracker.audiotracker_cli;
 
 import com.audiotracker.audiotracker_cli.AudiobookCLI;
 
+import com.audiotracker.audiotracker_cli.menu.CLIMenu;
+
 public class Main {
     public static void main(String[] args) {
-        AudiobookCLI cli = new AudiobookCLI();
-        cli.displayAllAudiobooks();
+        new CLIMenu().start();
     }
 }
 
